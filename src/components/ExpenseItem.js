becb9d49934998727c8ae1b083ded1217c4d7cbc
Waleed Anjum <waleedanjum936@gmail.com>
2023-01-05 10:51:24 +0500

    Custom Styling added for  Component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,15 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
-    return <h2>Expense Item here!</h2>
+  return (
+    <div className="expense-item">
+      <div>Jan 4th, 2023</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item__price">$300</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
