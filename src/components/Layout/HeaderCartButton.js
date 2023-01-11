668af53abeae21e272e16a1cc52dce3d1749c936
Waleed Anjum <waleedanjum936@gmail.com>
2023-01-11 17:22:43 +0500

    Header, Cart Button, Meals, Form, Shopping Cart, Modal via Portal, Cart Context, Cart Reducer, Refs & Forward Refs, Made items removable, useEffect Hook, Outputted Cart Items

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,47 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+  
+  //for re-evaluation and re-rendering of the component because we are changing btnClasses to include the bump animation class, and then setting a timer which removes that class again so that when it's added again in the future, it again plays.
+  const [btnIsHighlighted, setbtnIsHighlighted] = useState(false);
+
   const cartCtx = useContext(CartContext);
 
-  const numberOfCartItems = cartCtx.items.reduce((currNumber, item) => {
+  //object de-structuring because, if we add cartCtx as the dependency it would re-run whenever anything about cart context changes
+  const { items } = cartCtx;
+
+  const numberOfCartItems = items.reduce((currNumber, item) => {
     return currNumber + item.amount;
   }, 0);
 
+  //updating the state, the whole component will re-execute when state changes. So, this line will also change and updated classes will be picked down there
+  const btnClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ""
+  }`;
+
+  //effect function and dependencies array for bump animation class
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setbtnIsHighlighted(true);
+
+    //because bump was never removed and was fired only when first item was added in cart. So, an empty string will be added through const btnClass which will remove the bump class and will cause it to be added again whenever an item is added to cart.
+    const timer = setTimeout(() => {
+      setbtnIsHighlighted(false);
+    }, 300);
+
+    //cleanup function to clear timeout
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -8,13 +8,74 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
   if (action.type === "ADD_CART_ITEM") {
-    const updatedItems = state.items.concat(action.item);
-    const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+    //adding items as updated items instead of new items
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+
+    //finds index of item in an array
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+
+    //If item is already part of the array else existingCartItem will be NULL
+    const existingCartItem = state.items[existingCartItemIndex];
+    let updatedItems;
+
+    //if existingCartItem is truthy i.e already part of the array
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      };
+
+      //new array whith copied old objects without changing the old array
+      updatedItems = [...state.items];
+      //picked old item and overrided it with updatedItem
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      //when item is added for the first time
+      updatedItems = state.items.concat(action.item);
+    }
+
     return {
-        items: updatedItems,
-        totalAmount: updatedTotalAmount,
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
     };
   }
+
+  if (action.type === "REMOVE_CART_ITEM") {
+    
+    //finds index of item in an array
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+    const existingItem = state.items[existingCartItemIndex];
+
+    //Removing one item of a specific type by removing item entirely or just decreasing the amount
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
+
+    let updatedItems;
+    if (existingItem.amount === 1) {
+      
+      //all items where id !== action id are kept and for one where id === action id we remove that item
+      updatedItems = state.items.filter((item) => item.id !== action.id);
+    } else {
+      
+      //decrease item by 1
+      const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+
+      //copy of state items to create new array with old items
+      updatedItems = [...state.items];
+
+      //override old item with updated item
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+
   return defaultCartState;
 };
 
