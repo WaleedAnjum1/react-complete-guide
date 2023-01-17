32ded933465cef75e9284048dabe794a44a6bab8
Waleed Anjum <waleedanjum936@gmail.com>
2023-01-17 11:35:46 +0500

    Basic Validation, Validation Feedback, Handling (was touched) State, React to Lost Focus, Managing Overall Form Validity, Added Custom Input Hook, Re-Used Custom Hook, Applied Hook and Knowledge to a New Form

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,93 +1,120 @@
-import { useState } from "react";
+import useInput from "../hooks/use-input";
+
+const isNotEmpty = (value) => value.trim() !== "";
+const isEmail = (value) =>
+  /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value);
 
 const BasicForm = (props) => {
-  const [enteredFirstName, setEnteredFirstName] = useState("");
-  const [enteredLastName, setEnteredLastName] = useState("");
-  const [enteredMail, setEnteredMail] = useState("");
+  const {
+    value: firstNameValue,
+    isValid: enteredFirstNameIsValid,
+    hasError: firstNameInputHasError,
+    valueChangeHandler: firstNameInputChangeHandler,
+    inputBlurHandler: firstNameInputBlurHandler,
+    reset: resetFirstNameInput,
+  } = useInput(isNotEmpty);
+  const {
+    value: lastNameValue,
+    isValid: enteredLastNameIsValid,
+    hasError: lastNameInputHasError,
+    valueChangeHandler: lastNameInputChangeHandler,
+    inputBlurHandler: lastNameInputBlurHandler,
+    reset: resetLastNameInput,
+  } = useInput(isNotEmpty);
+  const {
+    value: mailValue,
+    isValid: enteredMailIsValid,
+    hasError: mailInputHasError,
+    valueChangeHandler: mailInputChangeHandler,
+    inputBlurHandler: mailInputBlurHandler,
+    reset: resetMailInput,
+  } = useInput(isEmail);
 
-  const [enteredFirstNameTouched, setEnteredFirstNameTouched] = useState(false);
-  const [enteredLastNameTouched, setEnteredLastNameTouched] = useState(false);
-  const [enteredMailTouched, setEnteredMailTouched] = useState(false);
+  // const [enteredFirstName, setEnteredFirstName] = useState("");
+  // const [enteredLastName, setEnteredLastName] = useState("");
+  // const [enteredMail, setEnteredMail] = useState("");
 
-  const enteredFirstNameIsValid = enteredFirstName.trim() !== "";
-  const enteredLastNameIsValid = enteredLastName.trim() !== "";
-  const enteredMailIsValid = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(
-    enteredMail
-  );
+  // const [enteredFirstNameTouched, setEnteredFirstNameTouched] = useState(false);
+  // const [enteredLastNameTouched, setEnteredLastNameTouched] = useState(false);
+  // const [enteredMailTouched, setEnteredMailTouched] = useState(false);
 
-  const firstNameInputIsInvalid = !enteredFirstNameIsValid && enteredFirstNameTouched;
-  const lastNameInputIsInvalid = !enteredLastNameIsValid && enteredLastNameTouched;
-  const MailInputIsInvalid = !enteredMailIsValid && enteredMailTouched;
+  // const enteredFirstNameIsValid = enteredFirstName.trim() !== "";
+  // const enteredLastNameIsValid = enteredLastName.trim() !== "";
+  // const enteredMailIsValid = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(
+  //   enteredMail
+  // );
 
+  // const firstNameInputIsInvalid =
+  //   !enteredFirstNameIsValid && enteredFirstNameTouched;
+  // const lastNameInputIsInvalid =
+  //   !enteredLastNameIsValid && enteredLastNameTouched;
+  // const MailInputIsInvalid = !enteredMailIsValid && enteredMailTouched;
+
+  // const firstNameInputChangeHandler = (event) => {
+  //   setEnteredFirstName(event.target.value);
+  // };
+
+  // const lastNameInputChangeHandler = (event) => {
+  //   setEnteredLastName(event.target.value);
+  // };
+
+  // const mailInputChangeHandler = (event) => {
+  //   setEnteredMail(event.target.value);
+  // };
+
+  // const firstNameInputBlurHandler = (event) => {
+  //   setEnteredFirstNameTouched(true);
+  // };
+
+  // const lastNameInputBlurHandler = (event) => {
+  //   setEnteredLastNameTouched(true);
+  // };
+
+  // const mailInputBlurHandler = (event) => {
+  //   setEnteredMailTouched(true);
+  // };
+  
   let formIsValid = false;
 
   if (enteredFirstNameIsValid && enteredLastNameIsValid && enteredMailIsValid) {
     formIsValid = true;
   }
 
-  const firstNameInputChangeHandler = (event) => {
-    setEnteredFirstName(event.target.value);
-  };
-
-  const lastNameInputChangeHandler = (event) => {
-    setEnteredLastName(event.target.value);
-  };
-
-  const mailInputChangeHandler = (event) => {
-    setEnteredMail(event.target.value);
-  };
-
-  const firstNameInputBlurHandler = (event) => {
-    setEnteredFirstNameTouched(true);
-  };
-
-  const lastNameInputBlurHandler = (event) => {
-    setEnteredLastNameTouched(true);
-  };
-
-  const mailInputBlurHandler = (event) => {
-    setEnteredMailTouched(true);
-  };
-
   //FORM HANDLER
   const formSubmissionHandler = (event) => {
     event.preventDefault();
 
-    setEnteredFirstNameTouched(true);
-    setEnteredLastNameTouched(true);
-    setEnteredMailTouched(true);
+    // setEnteredFirstNameTouched(true);
+    // setEnteredLastNameTouched(true);
+    // setEnteredMailTouched(true);
 
-    if (!enteredFirstNameIsValid) {
+    if (!formIsValid) {
       return;
     }
 
-    if (!enteredLastNameIsValid) {
-      return;
-    }
+    console.log(firstNameValue, lastNameValue, mailValue);
 
-    if (!enteredMailIsValid) {
-      return;
-    }
+    resetFirstNameInput();
+    resetLastNameInput();
+    resetMailInput();
 
-    console.log(enteredFirstName, enteredLastName, enteredMail);
+    // setEnteredFirstName("");
+    // setEnteredFirstNameTouched(false);
 
-    setEnteredFirstName("");
-    setEnteredFirstNameTouched(false);
+    // setEnteredLastName("");
+    // setEnteredLastNameTouched(false);
 
-    setEnteredLastName("");
-    setEnteredLastNameTouched(false);
-
-    setEnteredMail("");
-    setEnteredMailTouched(false);
+    // setEnteredMail("");
+    // setEnteredMailTouched(false);
   };
 
-  const firstNameInputClasses = firstNameInputIsInvalid
+  const firstNameInputClasses = firstNameInputHasError /*firstNameInputIsInvalid*/
     ? "form-control invalid"
     : "form-control";
-  const lastNameInputClasses = lastNameInputIsInvalid
+  const lastNameInputClasses = lastNameInputHasError
     ? "form-control invalid"
     : "form-control";
-  const mailInputClasses = MailInputIsInvalid
+  const mailInputClasses = mailInputHasError
     ? "form-control invalid"
     : "form-control";
 
@@ -101,9 +128,9 @@ const BasicForm = (props) => {
             id="firstname"
             onChange={firstNameInputChangeHandler}
             onBlur={firstNameInputBlurHandler}
-            value={enteredFirstName}
+            value={firstNameValue}
           />
-          {firstNameInputIsInvalid && (
+          {firstNameInputHasError && (
             <p className="error-text">First Name must not be Empty</p>
           )}
         </div>
@@ -114,9 +141,9 @@ const BasicForm = (props) => {
             id="lastname"
             onChange={lastNameInputChangeHandler}
             onBlur={lastNameInputBlurHandler}
-            value={enteredLastName}
+            value={lastNameValue}
           />
-          {lastNameInputIsInvalid && (
+          {lastNameInputHasError && (
             <p className="error-text">Last Name must not be Empty</p>
           )}
         </div>
@@ -128,9 +155,9 @@ const BasicForm = (props) => {
           id="email"
           onChange={mailInputChangeHandler}
           onBlur={mailInputBlurHandler}
-          value={enteredMail}
+          value={mailValue}
         />
-        {MailInputIsInvalid && <p className="error-text">Invalid Email</p>}
+        {mailInputHasError && <p className="error-text">Invalid Email</p>}
       </div>
       <div className="form-actions">
         <button disabled={!formIsValid}>Submit</button>
