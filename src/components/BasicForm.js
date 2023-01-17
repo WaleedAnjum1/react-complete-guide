1ccbb0dd02f20e5bba2783064e250c8c078bcbdb
Waleed Anjum <waleedanjum936@gmail.com>
2023-01-17 11:10:33 +0500

    Basic Validation, Validation Feedback, Handling (was touched) State, React to Lost Focus, Managing Overall Form Validity, Added Custom Input Hook, Re-Used Custom Hook, Applied Hook and Knowledge to a New Form

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -5,13 +5,25 @@ const BasicForm = (props) => {
   const [enteredLastName, setEnteredLastName] = useState("");
   const [enteredMail, setEnteredMail] = useState("");
 
-  const [enteredFirstNameIsValid, setEnteredFirstNameIsValid] = useState(false);
-  const [enteredLastNameIsValid, setEnteredLastNameIsValid] = useState(false);
-  const [enteredMailIsValid, setEnteredMailIsValid] = useState(false);
-
   const [enteredFirstNameTouched, setEnteredFirstNameTouched] = useState(false);
   const [enteredLastNameTouched, setEnteredLastNameTouched] = useState(false);
   const [enteredMailTouched, setEnteredMailTouched] = useState(false);
+
+  const enteredFirstNameIsValid = enteredFirstName.trim() !== "";
+  const enteredLastNameIsValid = enteredLastName.trim() !== "";
+  const enteredMailIsValid = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(
+    enteredMail
+  );
+
+  const firstNameInputIsInvalid = !enteredFirstNameIsValid && enteredFirstNameTouched;
+  const lastNameInputIsInvalid = !enteredLastNameIsValid && enteredLastNameTouched;
+  const MailInputIsInvalid = !enteredMailIsValid && enteredMailTouched;
+
+  let formIsValid = false;
+
+  if (enteredFirstNameIsValid && enteredLastNameIsValid && enteredMailIsValid) {
+    formIsValid = true;
+  }
 
   const firstNameInputChangeHandler = (event) => {
     setEnteredFirstName(event.target.value);
@@ -25,6 +37,18 @@ const BasicForm = (props) => {
     setEnteredMail(event.target.value);
   };
 
+  const firstNameInputBlurHandler = (event) => {
+    setEnteredFirstNameTouched(true);
+  };
+
+  const lastNameInputBlurHandler = (event) => {
+    setEnteredLastNameTouched(true);
+  };
+
+  const mailInputBlurHandler = (event) => {
+    setEnteredMailTouched(true);
+  };
+
   //FORM HANDLER
   const formSubmissionHandler = (event) => {
     event.preventDefault();
@@ -33,37 +57,29 @@ const BasicForm = (props) => {
     setEnteredLastNameTouched(true);
     setEnteredMailTouched(true);
 
-    if (enteredFirstName.trim() === "") {
-      setEnteredFirstNameIsValid(false);
-    }
-
-    if (enteredLastName.trim() === "") {
-      setEnteredLastNameIsValid(false);
-    }
-
-    if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(enteredMail)) {
-      setEnteredMailIsValid(false);
+    if (!enteredFirstNameIsValid) {
       return;
     }
 
-    setEnteredFirstNameIsValid(true);
-    setEnteredLastNameIsValid(true);
-    setEnteredMailIsValid(true);
+    if (!enteredLastNameIsValid) {
+      return;
+    }
+
+    if (!enteredMailIsValid) {
+      return;
+    }
 
     console.log(enteredFirstName, enteredLastName, enteredMail);
 
     setEnteredFirstName("");
+    setEnteredFirstNameTouched(false);
+
     setEnteredLastName("");
+    setEnteredLastNameTouched(false);
+
     setEnteredMail("");
+    setEnteredMailTouched(false);
   };
-
-  const firstNameInputIsInvalid =
-    !enteredFirstNameIsValid && enteredFirstNameTouched;
-
-  const lastNameInputIsInvalid =
-    !enteredLastNameIsValid && enteredLastNameTouched;
-    
-  const MailInputIsInvalid = !enteredMailIsValid && enteredMailTouched;
 
   const firstNameInputClasses = firstNameInputIsInvalid
     ? "form-control invalid"
@@ -84,6 +100,7 @@ const BasicForm = (props) => {
             type="text"
             id="firstname"
             onChange={firstNameInputChangeHandler}
+            onBlur={firstNameInputBlurHandler}
             value={enteredFirstName}
           />
           {firstNameInputIsInvalid && (
@@ -96,6 +113,7 @@ const BasicForm = (props) => {
             type="text"
             id="lastname"
             onChange={lastNameInputChangeHandler}
+            onBlur={lastNameInputBlurHandler}
             value={enteredLastName}
           />
           {lastNameInputIsInvalid && (
@@ -109,12 +127,13 @@ const BasicForm = (props) => {
           type="email"
           id="email"
           onChange={mailInputChangeHandler}
+          onBlur={mailInputBlurHandler}
           value={enteredMail}
         />
         {MailInputIsInvalid && <p className="error-text">Invalid Email</p>}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
